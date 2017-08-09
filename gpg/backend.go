b62109e9b644c3c0e590ca2ffca582cc7401b776
Thomas Gerbet <thomas@gerbet.me>
2017-08-09 23:13:16 +0200

    Init plugin

---
+++ gpg/backend.go
@@ -0,0 +1,28 @@
+package gpg
+
+import (
+	"github.com/hashicorp/vault/logical"
+	"github.com/hashicorp/vault/logical/framework"
+)
+
+func Factory(conf *logical.BackendConfig) (logical.Backend, error) {
+	b := Backend()
+	if err := b.Setup(conf); err != nil {
+		return nil, err
+	}
+	return b, nil
+}
+
+func Backend() *backend {
+	var b backend
+	b.Backend = &framework.Backend{
+		Help:        "GPG plugin",
+		Secrets:     []*framework.Secret{},
+		BackendType: logical.TypeLogical,
+	}
+	return &b
+}
+
+type backend struct {
+	*framework.Backend
+}
---
+++ main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"log"
+	"os"
+
+	"github.com/LeSuisse/vault-gpg-plugin/gpg"
+	"github.com/hashicorp/vault/helper/pluginutil"
+	"github.com/hashicorp/vault/logical/plugin"
+)
+
+func main() {
+	apiClientMeta := &pluginutil.APIClientMeta{}
+
+	tlsConfig := apiClientMeta.GetTLSConfig()
+	tlsProviderFunc := pluginutil.VaultPluginTLSProvider(tlsConfig)
+
+	err := plugin.Serve(&plugin.ServeOpts{
+		BackendFactoryFunc: gpg.Factory,
+		TLSProviderFunc:    tlsProviderFunc,
+	})
+	if err != nil {
+		log.Println(err)
+		os.Exit(1)
+	}
+}

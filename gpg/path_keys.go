5bae9b07bc90ea1a9a01c7c171bd216b43b47a67
Thomas Gerbet <thomas@gerbet.me>
2017-08-13 22:14:37 +0200

    A GPG key can be generated and read

--- gpg/path_keys.go
+++ gpg/path_keys.go
@@ -1,8 +1,13 @@
 package gpg
 
 import (
+	"bytes"
+	"encoding/hex"
+	"fmt"
 	"github.com/hashicorp/vault/logical"
 	"github.com/hashicorp/vault/logical/framework"
+	"golang.org/x/crypto/openpgp"
+	"golang.org/x/crypto/openpgp/armor"
 )
 
 func pathListKeys(b *backend) *framework.Path {
@@ -22,17 +27,109 @@ func pathKeys(b *backend) *framework.Path {
 				Type:        framework.TypeString,
 				Description: "Name of the key",
 			},
+			"real_name": {
+				Type:        framework.TypeString,
+				Description: "The real name of the identity associated with the generated GPG key.",
+			},
+			"email": {
+				Type:        framework.TypeString,
+				Description: "The email of the identity associated with the generated GPG key.",
+			},
+			"comment": {
+				Type:        framework.TypeString,
+				Description: "The comment of the identity associated with the generated GPG key.",
+			},
 		},
 		Callbacks: map[logical.Operation]framework.OperationFunc{
+			logical.ReadOperation:   b.pathKeyRead,
 			logical.UpdateOperation: b.pathKeyCreate,
 			logical.DeleteOperation: b.pathKeyDelete,
 		},
 	}
 }
 
+func (b *backend) key(s logical.Storage, name string) (*keyEntry, error) {
+	entry, err := s.Get("key/" + name)
+	if err != nil {
+		return nil, err
+	}
+	if entry == nil {
+		return nil, nil
+	}
+
+	var result keyEntry
+	if err := entry.DecodeJSON(&result); err != nil {
+		return nil, err
+	}
+
+	return &result, nil
+}
+
+func (b *backend) entity(s logical.Storage, name string) (*openpgp.Entity, error) {
+	entry, err := b.key(s, name)
+	if err != nil {
+		return nil, err
+	}
+	if entry == nil {
+		return nil, nil
+	}
+
+	r := bytes.NewReader(entry.SerializedKey)
+	el, err := openpgp.ReadKeyRing(r)
+	if err != nil {
+		return nil, err
+	}
+	if len(el) == 0 {
+		return nil, fmt.Errorf("PGP key does not contain an entity")
+	}
+
+	return el[0], nil
+}
+
+func (b *backend) pathKeyRead(req *logical.Request, data *framework.FieldData) (*logical.Response, error) {
+	entity, err := b.entity(req.Storage, data.Get("name").(string))
+	if err != nil {
+		return nil, err
+	}
+	if entity == nil {
+		return nil, nil
+	}
+
+	var buf bytes.Buffer
+	w, err := armor.Encode(&buf, openpgp.PublicKeyType, nil)
+	err = entity.Serialize(w)
+	w.Close()
+	if err != nil {
+		return nil, err
+	}
+
+	return &logical.Response{
+		Data: map[string]interface{}{
+			"fingerprint": hex.EncodeToString(entity.PrimaryKey.Fingerprint[:]),
+			"public_key":  buf.String(),
+		},
+	}, nil
+}
+
 func (b *backend) pathKeyCreate(req *logical.Request, data *framework.FieldData) (*logical.Response, error) {
 	name := data.Get("name").(string)
-	entry, err := logical.StorageEntryJSON("key/"+name, &keyEntry{})
+	realName := data.Get("real_name").(string)
+	email := data.Get("email").(string)
+	comment := data.Get("comment").(string)
+
+	entity, err := openpgp.NewEntity(realName, comment, email, nil)
+	if err != nil {
+		return nil, err
+	}
+	var buf bytes.Buffer
+	err = entity.SerializePrivate(&buf, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	entry, err := logical.StorageEntryJSON("key/"+name, &keyEntry{
+		SerializedKey: buf.Bytes(),
+	})
 	if err != nil {
 		return nil, err
 	}
@@ -60,4 +157,5 @@ func (b *backend) pathKeyList(
 }
 
 type keyEntry struct {
+	SerializedKey []byte
 }

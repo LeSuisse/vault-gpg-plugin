a63d15074b5740dbb29af05b6ef3cead1fbdd559
Thomas Gerbet <thomas@gerbet.me>
2021-12-28 11:33:47 +0100

    Build and test with Go 1.17

--- shell.nix
+++ shell.nix
@@ -1,7 +1,7 @@
 { pkgs ? import (
   fetchTarball {
-    url = "https://github.com/NixOS/nixpkgs/archive/dac74fead8737d6bf0823f22da26c7344f69bc0a.tar.gz";
-    sha256 = "0m0bqkh4bmlg5djb9bmyrpa1s4kwy2klil36487vyqkbdxnl76f7";
+    url = "https://github.com/NixOS/nixpkgs/archive/5c37ad87222cfc1ec36d6cd1364514a9efc2f7f2.tar.gz";
+    sha256 = "1r74afnalgcbpv7b9sbdfbnx1kfj0kp1yfa60bbbv27n36vqdhbb";
   }
 ) {} }:
 
@@ -10,6 +10,6 @@ pkgs.mkShell {
     pkgs.findutils
     pkgs.gitMinimal
     pkgs.zip
-    pkgs.go_1_16
+    pkgs.go_1_17
   ];
 }

2a1c965ea5289b74d5bd7d468a83d1445db09dc9
Thomas Gerbet <thomas@gerbet.me>
2023-06-03 21:57:00 +0200

    Update nix shell env

--- shell.nix
+++ shell.nix
@@ -1,7 +1,7 @@
 { pkgs ? import (
   fetchTarball {
-    url = "https://github.com/NixOS/nixpkgs/archive/7e0743a5aea1dc755d4b761daf75b20aa486fdad.tar.gz";
-    sha256 = "sha256:1zs1l3aivfwxmdrjvkiwqqj50z4yww5rx41m5b3qi6jv9pd9185r";
+    url = "https://github.com/NixOS/nixpkgs/archive/c6ffce3d5df7b4c588ce80a0c6e2d2348a611707.tar.gz";
+    sha256 = "sha256:15pdl53q06dsxwnix191mrkwzh21sb7yqrmz2qq6q25qk8hdpjrr";
   }
 ) {} }:
 

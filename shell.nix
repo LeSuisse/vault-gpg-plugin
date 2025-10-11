8e408521881fa7c5be12e85c39bc6b51b3fdbf4b
Thomas Gerbet <thomas@gerbet.me>
2025-10-11 11:28:29 +0200

    chore: Bump build/dev env

--- shell.nix
+++ shell.nix
@@ -1,13 +1,13 @@
 { pkgs ? import (
   fetchTarball {
-    url = "https://github.com/NixOS/nixpkgs/archive/69b9a8c860bdbb977adfa9c5e817ccb717884182.tar.gz";
-    sha256 = "sha256:12ljkkjg3gicamvryxr2bnfcdb05qdlbc5wv4lcw9sxamszp4cp7";
+    url = "https://github.com/NixOS/nixpkgs/archive/870493f9a8cb0b074ae5b411b2f232015db19a65.tar.gz";
+    sha256 = "sha256:045sqv2qym9hmly6c2khpbawwn26084x8lxz7qs0zqd5y9ahdjq4";
   }
 ) {} }:
 
 pkgs.mkShell {
   buildInputs = [
-    pkgs.go_1_23
+    pkgs.go_1_25
     pkgs.gitMinimal
     pkgs.goreleaser
     pkgs.syft
